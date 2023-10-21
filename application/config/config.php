5721513c193311fc7ddba0fcc54a16ba9bb5e42a
jhustyn <jhustynherwanap@gmail.com>
2023-10-21 11:06:38 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:8080/cms_pkkwu';
+$config['base_url'] = 'http://localhost:8080/cms-pkkwu';
 // $config['base_url'] = 'http://localhost/cms_pkkwu';
 
 /*
